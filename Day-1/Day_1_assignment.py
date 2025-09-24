0e697a78128e201aa65594f2b9860442c3c3acc3
Himanshu Chadha <chadha.himanshu.25@gmail.com>
2025-09-24 22:13:21 +0530

    day 1 assignment

---
+++ Day-1/Day_1_assignment.py
@@ -0,0 +1,30 @@
+# Q11. Rectangle Area Script
+
+# Program to calculate area of rectangle
+
+# Taking input from user
+length = float(input("Enter the length of rectangle: "))
+width = float(input("Enter the width of rectangle: "))
+
+# Calculating area
+area = length * width
+
+# Displaying result
+print("The area of rectangle is:", area)
+
+
+
+
+# Q12. Celsius to Fahrenheit Converter
+
+# Program to convert Celsius to Fahrenheit
+
+# Taking input from user
+celsius = float(input("Enter temperature in Celsius: "))
+
+# Conversion formula
+fahrenheit = (celsius * 9/5) + 32
+
+# Displaying result
+print("Temperature in Fahrenheit:", fahrenheit)
+
